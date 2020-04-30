0e15d2c866da22fba4241115d830d1bb5c6266e1
Paresh Sharma <Paresh.Sharma@Valaries-MBP.hsd1.il.comcast.net>
2020-04-30 12:06:42 -0500

    "User can sign up now"

--- resources/users.py
+++ resources/users.py
@@ -1,7 +1,13 @@
 import models
 
 #import blueprint
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from flask_bcrypt import generate_password_hash
+
+from playhouse.shortcuts import model_to_dict
+
+from flask_login import login_user
 
 # Create a blueprint
 users = Blueprint('users', 'users')
@@ -10,3 +16,75 @@ users = Blueprint('users', 'users')
 @users.route('/', methods=['GET'])
 def test_route():
     return "User route is working now"
+
+
+# Sign up route for user
+@users.route('/signup', methods=['POST'])
+def sign_up():
+    payload = request.get_json()
+
+    # Lower case all the user inputs
+    payload['name'] = payload['name'].lower()
+    payload['email'] = payload['email'].lower()
+    payload['username'] = payload['username'].lower()
+    print(payload)
+
+    try:
+        # Check if the user exist
+        models.User.get(models.User.username == payload['username'])
+
+        # if user's username already exist
+        return jsonify(
+            data={},
+            message=f"Username {payload['username']}  already taken",
+            status=401
+        ), 401
+
+
+    # handle ModelDoesNotExist error in the except
+    except models.DoesNotExist:
+
+        # hash the password with bcrypt
+        hashed_password = generate_password_hash(payload['password'])
+
+        # Create user
+        user_created = models.User.create(
+            name=payload['name'],
+            email=payload['email'],
+            username=payload['username'],
+            password=hashed_password
+        )
+
+        #This will start the session for the user
+        login_user(user_created)
+
+        # Convert it to dict for responce
+        user_created_dict = model_to_dict(user_created)
+
+        print(user_created_dict)
+
+        # Password can not be jsonify so we are gonna remove it while returning
+        user_created_dict.pop('password')
+
+        return jsonify(
+            data=user_created_dict,
+            message=f"New User with username {user_created_dict['username']} was created",
+            status = 201
+        ), 201
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
